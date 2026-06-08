d63a06f55bddc742da37cc45d8abb189dd8942ac
Ruchin Patel <ruchinpatel0203@gmail.com>
2026-06-08 19:32:29 +0530

    Add files via upload

---
+++ Chapter 5 - Dictionary and Sets/PS 5/problem7.py
@@ -0,0 +1,3 @@
+# 7. If the names of 2 friends are same; what will happen to the program in problem 6? 
+
+#Ans: the values entered later will be updated 
---
+++ Chapter 5 - Dictionary and Sets/PS 5/problem9.py
@@ -0,0 +1,7 @@
+# 9. Can you change the values inside a list which is contained in set S? s = {8, 7, 12, "Harry", [1,2]}
+
+s = {8, 7, 12, "Harry", [1,2]}
+
+s[4][0] = 8
+
+# It can't change. Because itrequire all their element to be immutable and hashable.
