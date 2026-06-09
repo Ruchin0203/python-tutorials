dcaa627bddb625d78fed69b43e0595dab5005f0f
Ruchin Patel <ruchinpatel0203@gmail.com>
2026-06-09 23:10:03 +0530

    Update problem6.py

--- Chapter 6 - Conditional Expression/PS 6/problem6.py
+++ Chapter 6 - Conditional Expression/PS 6/problem6.py
@@ -1,3 +1,12 @@
+# 6. Write a program to calculate the grade of a student from his marks from the 
+# following scheme: 
+# 90 – 100 => Ex 
+# 80 – 90 => A 
+# 70 – 80 => B 
+# 60 – 70  =>C 
+# 50 – 60 => D 
+# <50  => F 
+
 marks = int(input("Enter your marks:"))
 
 if(marks>90 and marks<=100):
