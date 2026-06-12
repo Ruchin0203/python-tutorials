8369e5a2368154366573351fcb411d4e706a6bf2
Ruchin Patel <ruchinpatel0203@gmail.com>
2026-06-12 18:33:19 +0530

    Add files via upload

---
+++ Chapter 8 - Functions and Recursions/function with arguments.py
@@ -0,0 +1,12 @@
+def GoodDay(name, ending):
+    print("Good Day, "+ name )
+    print(ending)
+    return "Done"  # return kare etle value male ane e variable ma store thay ,jo return na karo to variable ma none store thay.
+a = GoodDay("Ruchin", "Thank you!!")
+print(a)
+GoodDay("Raman", "Aabhaar")
+GoodDay("Rohan", "Thanks")
+# def greet(name):
+#     gr = "hello"+ name
+#     return gr
+# a = greet ("harry")
