b3014285f5dd4ec9d401c339d9cb88da8bc3e4be
Ruchin Patel <ruchinpatel0203@gmail.com>
2026-06-14 21:59:24 +0530

    Add files via upload

---
+++ Chapter 9 - File (Input-Output)/more_file_function.py
@@ -0,0 +1,28 @@
+f = open("file.txt","rt")
+data = f.readlines()
+print(data)
+
+# lines = f.readlines()
+# print(lines,type(lines))
+
+# lines1 = f.readline()
+# print(lines1,type(lines1))
+
+# lines2 = f.readline()
+# print(lines2,type(lines2))
+
+# lines3 = f.readline()
+# print(lines3,type(lines3))
+
+# lines4 = f.readline()
+# print(lines4,type(lines4))
+
+# lines5 = f.readline()
+# print(lines5,type(lines5))
+
+# line = f.readline()
+# while(line!=""):
+#     print(line)
+#     line=f.readline()
+
+# f.close()
