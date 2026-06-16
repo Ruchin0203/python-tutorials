5d3279df5569b8297cc193b7d40eddcac471bed9
Ruchin Patel <ruchinpatel0203@gmail.com>
2026-06-16 18:16:14 +0530

    Add files via upload

---
+++ Project 1 - Snake, Water, Gun Game/shortened.py
@@ -0,0 +1,43 @@
+import random
+"""
+1 for snake 
+-1 for water 
+0 for gun
+"""
+computer = random.choice([-1, 0, 1])
+youstr = input("Entre your choice: ")
+youDict = {"s": 1,"w": -1, "g": 0}
+reverseDict = { 1: "Snake", -1: "Water", 0:"Gun"}
+
+you = youDict[youstr]
+
+print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
+
+
+if(you == computer):
+    print("it's a draw!")
+
+else:
+    '''
+    if(computer==-1 and you==1): (computer - you) = -2
+        print("You win!")
+    elif(computer==-1 and you==0): (computer - you) = -1
+        print("You loos!")
+    elif(computer==1 and you==0): (computer - you) = 1
+        print("You win!")
+    elif(computer==1 and you==-1): (computer - you) = 2
+        print("You loos!")
+    elif(computer==0 and you==-1): (computer - you) = 1
+        print("You win!")
+    elif(computer==0 and you==1): (computer - you) = -1
+        print("You loos!")
+    else:
+        print("something went wrong!")
+
+    below logic is written on the basis of the value of (computer - you)
+
+    '''
+    if((computer - you) == -1 or (computer - you) == 2):
+        print("You loos!")
+    else:
+        print("You win!")
