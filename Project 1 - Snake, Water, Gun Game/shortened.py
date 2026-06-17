f60e80f72f61e5bb440288b4f781b930977c8272
Ruchin Patel <ruchinpatel0203@gmail.com>
2026-06-17 19:27:59 +0530

    Update shortened.py

--- Project 1 - Snake, Water, Gun Game/shortened.py
+++ Project 1 - Snake, Water, Gun Game/shortened.py
@@ -40,4 +40,4 @@ else:
     if((computer - you) == -1 or (computer - you) == 2):
         print("You loos!")
     else:
-        print("You win!")
+        print("You win!")  # this logic reduse code's length
